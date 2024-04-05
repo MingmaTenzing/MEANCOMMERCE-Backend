c7a7e2a15cd39ae280a314184ce8d50abae039f0
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-04-05 11:05:59 +0800

    supabase is working

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 //required default
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
 require("dotenv").config();
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //routes
 const productsRoute = require("./routes/products");
@@ -14,12 +17,14 @@ const port = 5000;
 
 //DB
 const connectDB = require("./db/connect");
+
 app.use("/api/v1/categories", categoriesRoute);
 app.use("/api/v1/products", productsRoute);
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
+
     app.listen(port, () => console.log("listening on port " + port));
   } catch (error) {
     console.log(error);
--- controllers/categories.js
+++ controllers/categories.js
@@ -1,5 +1,14 @@
-const getAllCategories = (req, res) => {
-  res.send("All categories page");
+const supabase = require("../db/supabase");
+
+const getAllCategories = async (req, res) => {
+  try {
+    const { data } = await supabase.from("test").select("*");
+    if (data) {
+      res.json({ data });
+    }
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 module.exports = getAllCategories;
