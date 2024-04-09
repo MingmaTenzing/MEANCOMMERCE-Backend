1d84b802bd7dff5b4c75c91b0dc450d014c9b6e7
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-04-09 11:35:34 +0800

    working on product model

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,14 +1,35 @@
-const supabase = require("../db/supabase");
-
+// const supabase = require("../db/supabase");
+// const image = require("../ming.jpg");
 const getAllCategories = async (req, res) => {
-  try {
-    const { data } = await supabase.from("test").select("*");
-    if (data) {
-      res.json({ data });
-    }
-  } catch (err) {
-    console.error(err);
-  }
+  // try {
+  //   const { data, error } = await supabase
+  //     .from("test")
+  //     .insert({
+  //       name: "mingma Tenzing Sherpa loves you",
+  //     })
+  //     .select("*");
+  //   res.json({ data });
+  // } catch (err) {}
+
+  //   try {
+  //     const { data, error } = await supabase.storage
+  //       .from("Product Images")
+  //       .upload("/public/product", image, {
+  //         cacheControl: "3600",
+  //         upsert: false,
+  //       });
+  //   } catch (error) {}
+  // };
+
+  res.json({
+    categories: [
+      "smartphone",
+      "computers",
+      "gamingconsoles",
+      "accessories",
+      "headphones",
+    ],
+  });
 };
 
 module.exports = getAllCategories;
