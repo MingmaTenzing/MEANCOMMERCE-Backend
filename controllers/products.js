4fb48cc6cf5107b79327a5c8a4382072f1f6f411
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-05-14 10:15:27 +0800

    single product request complete

--- controllers/products.js
+++ controllers/products.js
@@ -1,47 +1,101 @@
 const Product = require("../models/product");
-const headphones = require("../headphone.json");
-const tv = require("../tv.json");
 const { StatusCodes } = require("http-status-codes");
-const camera = require("../camera.json");
+
 const getAllProducts = async (req, res) => {
   const products = await Product.find({});
-  res.json(products);
+  if (!products) {
+    res.status(StatusCodes.NOT_FOUND).json({
+      message: "Product not Found",
+    });
+  }
+  res.status(StatusCodes.OK).json(products);
+};
+
+const getSingleProduct = async (req, res) => {
+  const productId = req.params.id;
+  if (!productId) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "Please provide the product ID" });
+  }
+
+  try {
+    const product = await Product.findOne({ _id: productId });
+    res.status(StatusCodes.OK).json(product);
+  } catch (error) {
+    if (error.name == "CastError") {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .send({ message: "Product not found with id " + productId });
+    }
+  }
+
+  // const product = await Product.findOne({ _id: productId });
+  // if (!product) {
+  //   res
+  //     .status(StatusCodes.NOT_FOUND)
+  //     .json({ msg: "product not found with id " + productId });
+  // }
+  // res.json({ product });
 };
 
 const uploadProduct = async (req, res) => {
   const product = await Product.create(camera);
+  if (!product) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: "Internal Server Error",
+    });
+  }
   res.status(StatusCodes.OK).json({
     product,
   });
-  res.json({ product }); // const product = await Product.create(smartphones);
-  // res.status(200).json({
-  //   product,
-  // });
-  // res.json({ product });
 };
+
 const smartPhone = async (req, res) => {
   const smartPhones = await Product.find({ category: "Smartphone" });
-  res.json(smartPhones);
+  if (!smartPhones) {
+    res.status(StatusCodes.NOT_FOUND).json({
+      message: "Product not Found",
+    });
+  }
+  res.status(StatusCodes.OK).json(smartPhones);
 };
-const headPhones = (req, res) => {
-  res.send("headphone Category");
+
+const headPhones = async (req, res) => {
+  const products = await Product.find({ category: "Headphone" });
+  if (!products) {
+    res.status(StatusCodes.NOT_FOUND).json({
+      message: "Product not Found",
+    });
+  }
+  res.status(StatusCodes.OK).json(products);
 };
-const computers = (req, res) => {
-  res.send("computer Category");
+const computers = async (req, res) => {
+  const products = await Product.find({ category: "Computer & Laptop" });
+  if (!products) {
+    res.status(StatusCodes.NOT_FOUND).json({
+      message: "Product not Found",
+    });
+  }
+  res.status(StatusCodes.OK).json(products);
 };
-const gamingConsoles = (req, res) => {
-  res.send("gamingconsole Category");
-};
-const accesssories = (req, res) => {
-  res.send("accessories Category");
+
+const accesssories = async (req, res) => {
+  const products = await Product.find({ category: "Accessories" });
+  if (!products) {
+    res.status(StatusCodes.NOT_FOUND).json({
+      message: "Product not Found",
+    });
+  }
+  res.status(StatusCodes.OK).json(products);
 };
 
 module.exports = {
   smartPhone,
   computers,
-  gamingConsoles,
   accesssories,
   headPhones,
   getAllProducts,
   uploadProduct,
+  getSingleProduct,
 };
--- routes/products.js
+++ routes/products.js
@@ -7,15 +7,15 @@ const {
   headPhones,
   computers,
   accesssories,
-  gamingConsoles,
   uploadProduct,
+  getSingleProduct,
 } = require("../controllers/products");
 
 router.route("/").get(getAllProducts).post(uploadProduct);
+router.route("/:id").get(getSingleProduct);
 router.route("/smartphones").get(smartPhone);
 router.route("/headphones").get(headPhones);
 router.route("/computers").get(computers);
 router.route("/accessories").get(accesssories);
-router.route("/gamingconsoles").get(gamingConsoles);
 
 module.exports = router;
