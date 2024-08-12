5978d592c47c65d52fb9fb606310097117b8ac14
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-08-12 09:57:26 +0800

    error handling in stirpe checkout

--- controllers/checkout-session.js
+++ controllers/checkout-session.js
@@ -6,22 +6,32 @@ const stripe = require("stripe")(stripe_secret);
 const checkoutSession = async (req, res) => {
   const { line_items } = req.body;
 
-  console.log(line_items);
-  const session = await stripe.checkout.sessions.create({
-    line_items: line_items,
-    mode: "payment",
-    ui_mode: "embedded",
-    return_url: `http://localhost:4200/success?session_id={CHECKOUT_SESSION_ID}`,
-  });
+  if (!line_items) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "Please provide line items" });
+  }
 
-  res.status(StatusCodes.OK).json(session.client_secret);
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: line_items,
+      mode: "payment",
+      ui_mode: "embedded",
+      return_url: `http://localhost:4200/success?session_id={CHECKOUT_SESSION_ID}`,
+    });
+
+    res.status(StatusCodes.OK).json(session.client_secret);
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error });
+  }
 };
 
 const sessionStatus = async (req, res) => {
   const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
-  res.json({
+  res.status(StatusCodes.OK).json({
     status: session.status,
     customer_email: session.customer_details.email,
+    customer_name: session.customer_details.name,
   });
 };
 
