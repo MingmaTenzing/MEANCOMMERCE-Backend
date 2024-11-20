df407ad3710b26f502033eb7ee79b6f8dcc16bad
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mingmas-Mac-mini.local>
2024-11-20 11:55:14 +0800

    logout auth route is now available was missing before

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,13 @@ const {
   register,
   login,
   check_auth_sesion,
+  sign_out,
 } = require("../controllers/authentication");
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/logout").post(sign_out);
 router.route("/auth-check-session").get(check_auth_sesion);
 
 module.exports = router;
