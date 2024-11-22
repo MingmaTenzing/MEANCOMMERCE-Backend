8ded7684eaf62707bf91aaf6c552316035b290b0
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mingmas-Mac-mini.local>
2024-11-23 07:13:14 +0800

    sorted the orders list on fetch

--- controllers/orders.js
+++ controllers/orders.js
@@ -3,7 +3,9 @@ const { StatusCodes } = require("http-status-codes");
 
 const get_recent_orders = async (req, res) => {
   try {
-    const findOrders = await Orders.find({ user_id: req.user.userId });
+    const findOrders = await Orders.find({ user_id: req.user.userId }).sort({
+      created_at: -1,
+    });
     res.status(StatusCodes.OK).json(findOrders);
   } catch (error) {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
