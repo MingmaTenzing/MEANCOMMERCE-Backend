77c10b0b9fcc11071e1c5d9560e1ab0ebf8d0499
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mingmas-Mac-mini.local>
2024-12-09 17:45:48 +0800

    open ai is taking the query from req.body

--- controllers/openai.js
+++ controllers/openai.js
@@ -3,6 +3,8 @@ const baseURL = "https://api.aimlapi.com/v1";
 const open_ai = new OpenAI({ apiKey: process.env.AI_ML_API_SECRET, baseURL });
 
 const openai_suggestion = async (req, res) => {
+  const { query } = req.body;
+
   const response = await open_ai.chat.completions.create({
     model: "mistralai/Mistral-7B-Instruct-v0.2",
     messages: [
@@ -13,7 +15,7 @@ const openai_suggestion = async (req, res) => {
       },
       {
         role: "user",
-        content: "Which is better iphone 14 pro or pixel 8",
+        content: query,
       },
     ],
   });
